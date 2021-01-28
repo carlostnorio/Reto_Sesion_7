5f372adf0790e8d6b145f994ebcd4100f9ad1761
Carlos Roríguez <jocarote@hotmail.com>
2021-01-28 02:19:13 +0000

    #Reto 1 - Sesión 7 - Fase II BEDU SANTANDER

---
+++ queries.R
@@ -0,0 +1,36 @@
+
+library(dplyr)
+library(DBI)
+library(RMySQL)
+library(ggplot2)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+
+#Porcentaje de personas que hablan español
+dbListFields(MyDataBase, 'CountryLanguage')
+
+Español <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+
+DataEspañol <- Español %>% filter(Language == "Spanish")
+head(DataEspañol)
+class(DataEspañol)
+
+DataEspañol %>% ggplot() + 
+  aes(x=CountryCode, y=Percentage, color=IsOfficial) +
+  ylab("Porcentaje") +
+  xlab("Código de pais") + 
+  theme_light()
+
+DataEspañol %>% ggplot(aes(x =CountryCode, y=Percentage, fill = IsOfficial)) + 
+  geom_bin2d() +
+  coord_flip() +
+  ylab("Porcentaje") +
+  xlab("Código de pais") + 
+  theme_light()
